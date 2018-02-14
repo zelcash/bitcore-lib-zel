2f4c11cf8697e10f05f0e07fa5eabf922ff39be1
Lumiboy <tha_lumi@hotmail.com>
2018-02-14 13:01:26 +0100

    BTCZ to ZEL

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ var bitcore = module.exports;
 bitcore.version = 'v' + require('./package.json').version;
 bitcore.versionGuard = function(version) {
   if (version !== undefined) {
-    var message = 'More than one instance of bitcore-lib-btcz found. ' +
-      'Please make sure to require bitcore-lib-btcz and check that submodules do' +
-      ' not also include their own bitcore-lib-btcz dependency.';
+    var message = 'More than one instance of bitcore-lib-zel found. ' +
+      'Please make sure to require bitcore-lib-zel and check that submodules do' +
+      ' not also include their own bitcore-lib-zel dependency.';
     throw new Error(message);
   }
 };
-bitcore.versionGuard(global._bitcoreBtcz);
-global._bitcoreBtcz = bitcore.version;
+bitcore.versionGuard(global._bitcoreZel);
+global._bitcoreZel = bitcore.version;
 
 // crypto
 bitcore.crypto = {};
